951e1811a3a53a133537d31118f842080ffb838e
treydock <tdockendorf@osc.edu>
2021-11-08 12:26:14 -0500

    Support Ubuntu 20.04 Fixes #102

--- spec/acceptance/openondemand_spec.rb
+++ spec/acceptance/openondemand_spec.rb
@@ -4,7 +4,10 @@ describe 'openondemand class:' do
   context 'default parameters' do
     it 'runs successfully' do
       pp = <<-EOS
-      class { 'openondemand': }
+      class { 'openondemand':
+        # TODO: Switch to 2.0 repo once it has Ubuntu packages
+        repo_release => 'build/2.0',
+      }
       EOS
 
       apply_manifest(pp, catch_failures: true)
@@ -25,13 +28,17 @@ describe 'openondemand class:' do
       apply_manifest(pp, catch_changes: true)
     end
 
-    describe yumrepo('ondemand-web-nightly') do
+    describe yumrepo('ondemand-web-nightly'), if: fact('os.family') == 'RedHat' do
       it { is_expected.to be_enabled }
     end
 
-    describe command('rpm -q ondemand') do
+    describe command('rpm -q ondemand'), if: fact('os.family') == 'RedHat' do
       its(:exit_status) { is_expected.to eq 0 }
       its(:stdout) { is_expected.to match(%r{nightly}) }
+    end
+
+    describe file('/etc/apt/sources.list.d/ondemand-web-nightly.list'), if: fact('os.family') == 'Debian' do
+      it { is_expected.to be_file }
     end
   end
 end
