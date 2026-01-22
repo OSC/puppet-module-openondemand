7180718d0479cdfa854ebaab0d9e2ab7bab2081a
Trey Dockendorf <tdockendorf@osc.edu>
2026-01-22 11:03:20 -0500

    Only test OnDemand 4.1 with EL10

--- spec/spec_helper_acceptance_setup.rb
+++ spec/spec_helper_acceptance_setup.rb
@@ -4,12 +4,27 @@ install_module_from_forge('puppetlabs-concat', '>= 9.0.0 < 10.0.0')
 install_module_from_forge('puppet-augeasproviders_core', '>= 3.0.0 < 4.0.0')
 on hosts, 'puppet config set strict warning'
 
+def supports_41_only
+  if fact('os.release.major').to_i >= 10 && (fact('os.family') == 'RedHat' && fact('os.name') != 'Amazon')
+    true
+  else
+    false
+  end
+end
+
 def supported_releases
-  {
-    '4.0' => ['4.0.5', 'latest'],
-    # TODO: Reset back to just 4.1.0 once that version is tagged
-    '4.1' => ['>=4.1.0', 'latest'],
-  }
+  if supports_41_only
+    {
+      # TODO: Reset back to just 4.1.0 once that version is tagged
+      '4.1' => ['>=4.1.0', 'latest'],
+    }
+  else
+    {
+      '4.0' => ['4.0.5', 'latest'],
+      # TODO: Reset back to just 4.1.0 once that version is tagged
+      '4.1' => ['>=4.1.0', 'latest'],
+    }
+  end
 end
 
 RSpec.configure do |c|
